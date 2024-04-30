bd5d7656e9dc428336a8d8dbceb06c1caa0eaf35
Shariar Islam <shariarn85@gmail.com>
2024-04-30 22:36:09 +0600

    Object , Optional And Literal Types

---
+++ src/object-ptional-literal-types.ts
@@ -0,0 +1,12 @@
+// Reference Type ==> Object
+const user: {
+  firstName: string;
+  middleName: string;
+  lastName?: string; //Optional Types
+  companyName: "Programming Hero"; //Literal Types
+} = {
+  firstName: "Shariar",
+  middleName: "Islam",
+  lastName: "Niaj",
+  companyName: "Programming Hero",
+};
