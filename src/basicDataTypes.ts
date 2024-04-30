67330972316baf27ad816006431e3128bcbdf600
Shariar Islam <shariarn85@gmail.com>
2024-04-30 22:22:23 +0600

    Basic Data Types

---
+++ src/basicDataTypes.ts
@@ -0,0 +1,21 @@
+// Basic Data Type
+let firstName: string = "Shariar";
+
+let roll: number = 17;
+
+let isAdmin: boolean = true;
+
+let x: undefined = undefined;
+
+let y: null = null;
+
+// Array
+let friends: string[] = ["Sharia", "Islam"];
+
+let eligibleRollList: number[] = [1, 2, 3, 5];
+
+// Tuple
+
+let coordinates: [number, number] = [1, 5];
+let ageName: [number, string, boolean] = [50, "Name", true];
+ageName[0] = 67;
