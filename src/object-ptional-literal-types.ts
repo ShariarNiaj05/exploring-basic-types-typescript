fa39a3660eae5ad2d74e9c1364fba91ed219a6e7
Shariar Islam <shariarn85@gmail.com>
2024-05-02 00:18:04 +0600

    updated object-ptional-literal-types

--- src/object-ptional-literal-types.ts
+++ src/object-ptional-literal-types.ts
@@ -1,12 +1,19 @@
-// Reference Type ==> Object
+{
+  
+  
+  // Reference Type ==> Object
 const user: {
   firstName: string;
   middleName: string;
   lastName?: string; //Optional Types
-  companyName: "Programming Hero"; //Literal Types
+  readonly companyName: "Programming Hero"; //Literal Types
 } = {
   firstName: "Shariar",
   middleName: "Islam",
   lastName: "Niaj",
   companyName: "Programming Hero",
-};
+  };
+  
+/* user.companyName = "xyz" //Cannot assign to 'companyName' because it is a read-only property.ts */
+
+}
